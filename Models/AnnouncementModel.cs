e6c3215983b54282864a26200a778e6a0fc0447f
Stas Oleksyn <shurikan189@gmail.com>
2024-08-14 16:04:59 +0300

    Initial commit

---
+++ Models/AnnouncementModel.cs
@@ -0,0 +1,10 @@
+﻿namespace nerdysoft_TestTask.Models
+{
+    public class AnnouncementModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
